86e345185f996cc7f5ae502691d582ae62133f3f
Michael Spence (Cefas) <michael.spence@cefas.co.uk>
2019-05-30 15:29:01 +0100

    Initialising the git site, all a bit late

---
+++ R/other.R
@@ -0,0 +1,8 @@
+#' @rawNamespace useDynLib(LeMaRns, .registration=TRUE)
+#' @importFrom Rcpp evalCpp
+#' @importFrom abind abind
+#' @importFrom grDevices rainbow
+#' @importFrom graphics layout legend lines par plot
+#' @importFrom methods new
+#' @importFrom stats dlnorm weighted.mean
+NULL
